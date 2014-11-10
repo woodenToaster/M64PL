1d23a1c3cedb173c46bf742668f7009d115da4f2
woodenToaster <litmusik@yahoo.com>
2014-11-10 14:48:40 -0600

    Clean up directory structure

--- M64PL/testPipeline.py
+++
@@ -1,9 +0,0 @@
-import cutest as unittest
-import re
-from pipeline import Pipeline
-
-class TestPipeline(unittest.TestCase):
-
-	def testPopulateIRegs(self):
-		pipeline = Pipeline('project-input.0.txt')
-		self.assertEqual(pipeline.IRegs, {'R2': 16})
